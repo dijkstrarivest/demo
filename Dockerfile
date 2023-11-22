e3405771daa8e1ffa0d4a2b5c75c7690e805a538
João Paulo F. C. César <joaocesar@souunilavras.com>
2023-11-22 17:59:12 -0300

    Commit inicial

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Build Stage
+FROM ubuntu:latest AS build
+
+RUN apt-get update
+RUN apt-get install openjdk-17-jdk -y
+RUN apt-get install maven -y
+
+WORKDIR /app
+COPY . .
+RUN mvn clean install
+
+# Production Stage
+FROM openjdk:17-jdk-slim
+
+EXPOSE 8080
+
+WORKDIR /app
+COPY --from=build /app/target/demo-0.0.1-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/demo/model/Produto.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+
+    // getters e setters
+    
+    // Getter e Setter para id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter e Setter para nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter e Setter para preco
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
